b8bf8bc579db1ad27868b344014ea6a3138457e9
VBalazs <v.vbalazs@gmail.com>
2020-12-02 22:20:48 +0100

    van benne bootstrap

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
 <!--most itt van -->
 <!--talán?-->
 <!--talán? ez el fogadható chatnek?!-->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Ki nevet a végén</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <link href="../OraiMunkaVideo/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
+        <div>
+            <h1>Kezdetek</h1>
+        </div>
         <?php
         // put your code here
         ?>
